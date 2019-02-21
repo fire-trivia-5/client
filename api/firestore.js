9fdec3ed15762c1e9e7f2b42865b30262658a421
Celyn Susanto <celyn.vy@gmail.com>
2019-02-21 18:43:48 +0700

    add some config

---
+++ api/firestore.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+require('dotenv').config()
+
+let config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+}
+
+firebase.initializeApp(config)
+
+let db = firebase.firestore()
+
+export default db
